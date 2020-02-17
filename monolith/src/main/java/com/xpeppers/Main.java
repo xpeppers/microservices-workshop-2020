c0e69acce28fb139c1034e59cb898ef4d522a2bc
danielefongo <danielefongo@gmail.com>
2020-02-17 12:08:22 +0100

    [branch-by-abstraction] switch implementation

--- monolith/src/main/java/com/xpeppers/Main.java
+++ monolith/src/main/java/com/xpeppers/Main.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-        Notifier notifier = new MonolithNotifier();
+        Notifier notifier = new MicroserviceNotifier();
         OrderRepository orderRepository = new OrderRepository();
 
         OrderAPI orderAPI = orderAPI(orderRepository, notifier);
