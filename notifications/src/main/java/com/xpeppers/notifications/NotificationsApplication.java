a150de6b4b0f5b221cad1959ad90c4649dcf019c
danielefongo <danielefongo@gmail.com>
2020-02-18 12:28:09 +0100

    [service-discovery] register notifications service to eureka server

--- notifications/src/main/java/com/xpeppers/notifications/NotificationsApplication.java
+++ notifications/src/main/java/com/xpeppers/notifications/NotificationsApplication.java
@@ -2,7 +2,11 @@ package com.xpeppers.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableDiscoveryClient
+@EnableRetry
 @SpringBootApplication
 public class NotificationsApplication {
 
